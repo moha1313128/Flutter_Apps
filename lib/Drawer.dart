ce60f964123e906260c9f97b8219de12982d6361
moha1313128 <moha1313128@gmail.com>
2020-04-03 21:03:13 +0100

    Create AppBar & Carousel & Bottom Navigation Tab & Drawer

---
+++ lib/Drawer.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:getflutter/components/avatar/gf_avatar.dart';
+import 'package:getflutter/components/drawer/gf_drawer.dart';
+import 'package:getflutter/components/drawer/gf_drawer_header.dart';
+
+class DrawerApp extends StatefulWidget {
+  DrawerApp({Key key}) : super(key: key);
+
+  @override
+  _DrawerAppState createState() => _DrawerAppState();
+}
+
+class _DrawerAppState extends State<DrawerApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: GFDrawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            GFDrawerHeader(
+              currentAccountPicture: GFAvatar(
+                radius: 80.0,
+                backgroundImage: NetworkImage(
+                    "https://cdn.pixabay.com/photo/2017/12/03/18/04/christmas-balls-2995437_960_720.jpg"),
+              ),
+              otherAccountsPictures: <Widget>[
+                Image(
+                  image: NetworkImage(
+                      "https://cdn.pixabay.com/photo/2019/12/20/00/03/road-4707345_960_720.jpg"),
+                  fit: BoxFit.cover,
+                ),
+                GFAvatar(
+                  child: Text("ab"),
+                )
+              ],
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('user name'),
+                  Text('user@userid.com'),
+                ],
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: null,
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: null,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
