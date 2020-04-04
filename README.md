2b340bbfe300466fad34ad2938110f88c9b3e25b
moha1313128 <moha1313128@gmail.com>
2020-04-04 13:16:58 +0100

    Building UI

--- README.md
+++ README.md
@@ -14,4 +14,25 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
-"# Flutter_Apps" 
+
+# Tutorial
+https://github.com/ionicfirebaseapp/Flutter-firestore-auth
+https://medium.com/flutterpub/flutter-how-to-do-user-login-with-firebase-a6af760b14d5
+https://github.com/tattwei46/flutter_login_demo
+https://flutterawesome.com/provides-login-screen-with-login-signup-functionalities/
+
+## Get SHA1 from debug.keystore file
+```
+keytool -list -v -keystore debug.keystore -alias androiddebugkey -storepass android -keypass android
+```
+## Create debug.keystore file again at the same location if required.
+```
+keytool -genkey -v -keystore debug.keystore -storepass android -alias androiddebugkey -keypass android -keyalg RSA -keysize 2048 -validity 10000
+```
+
+## cmd
+```
+keytool -list -v -keystore debug.keystore -alias androiddebugkey -storepass android -keypass android
+```
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,222 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/Drawer.dart';
-import 'package:getflutter/components/appbar/gf_appbar.dart';
-import 'package:getflutter/components/avatar/gf_avatar.dart';
-import 'package:getflutter/components/button/gf_icon_button.dart';
-import 'package:getflutter/components/card/gf_card.dart';
-import 'package:getflutter/components/carousel/gf_carousel.dart';
-import 'package:getflutter/components/drawer/gf_drawer.dart';
-import 'package:getflutter/components/drawer/gf_drawer_header.dart';
-import 'package:getflutter/components/list_tile/gf_list_tile.dart';
-import 'package:getflutter/components/loader/gf_loader.dart';
-import 'package:getflutter/components/tabs/gf_segment_tabs.dart';
-import 'package:getflutter/components/tabs/gf_tabbar.dart';
-import 'package:getflutter/types/gf_button_type.dart';
-import 'package:getflutter/types/gf_loader_type.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_app/screens/home/home.dart';
+import 'styles/styles.dart';
+import 'screens/home/first-screen.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  runApp(new MyApp());
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    systemNavigationBarColor: Colors.white,
+//    statusBarColor: Colors.transparent,
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: primaryDark,
+        accentColor: primaryDark,
+        cursorColor: Colors.black,
+        backgroundColor: primaryDark,
+        unselectedWidgetColor: Colors.grey,
       ),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  HomePage({Key key}) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Business',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  final List<String> imageList = [
-    "https://cdn.pixabay.com/photo/2017/12/03/18/04/christmas-balls-2995437_960_720.jpg",
-    "https://cdn.pixabay.com/photo/2017/12/13/00/23/christmas-3015776_960_720.jpg",
-    "https://cdn.pixabay.com/photo/2019/12/19/10/55/christmas-market-4705877_960_720.jpg",
-    "https://cdn.pixabay.com/photo/2019/12/20/00/03/road-4707345_960_720.jpg",
-    "https://cdn.pixabay.com/photo/2019/12/22/04/18/x-mas-4711785__340.jpg",
-    "https://cdn.pixabay.com/photo/2016/11/22/07/09/spruce-1848543__340.jpg"
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    var GFType;
-    return Scaffold(
-      drawer: Drawer(
-          child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          GFDrawerHeader(
-            currentAccountPicture: GFAvatar(
-              radius: 80.0,
-              backgroundImage: NetworkImage(
-                  "https://cdn.pixabay.com/photo/2017/12/03/18/04/christmas-balls-2995437_960_720.jpg"),
-            ),
-            otherAccountsPictures: <Widget>[
-              Image(
-                image: NetworkImage(
-                    "https://cdn.pixabay.com/photo/2019/12/20/00/03/road-4707345_960_720.jpg"),
-                fit: BoxFit.cover,
-              ),
-              GFAvatar(
-                child: Text("ME"),
-              )
-            ],
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('Mohammed Ettayby'),
-                Text('mohammed.ettaibi@edu.uca.ma'),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 60.0),
-            child: ListTile(
-              title: Text('Notification'),
-              onTap: null,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 60.0),
-            child: ListTile(
-              title: Text('Reviwes'),
-              onTap: null,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 60.0),
-            child: ListTile(
-              title: Text('Payments'),
-              onTap: null,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 60.0),
-            child: ListTile(
-              title: Text('Settings'),
-              onTap: null,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 60.0),
-            child: ListTile(
-              title: Text('Profile'),
-              onTap: null,
-            ),
-          ),
-          Divider(),
-          Padding(
-            padding: const EdgeInsets.only(left: 60.0),
-            child: ListTile(
-              title: Text('Log out'),
-              onTap: null,
-            ),
-          ),
-        ],
-      )),
-      appBar: GFAppBar(
-        // backgroundColor: Colors.white,
-        leading: GFIconButton(
-          icon: Icon(
-            Icons.menu,
-            color: Colors.white,
-          ),
-          onPressed: () {
-            Scaffold.of(context).openDrawer();
-          },
-          type: GFButtonType.transparent,
-        ),
-        searchBar: true,
-        title: Text("Shopping"),
-        actions: <Widget>[
-          GFIconButton(
-            icon: Icon(
-              Icons.favorite,
-              color: Colors.white,
-            ),
-            onPressed: () {},
-            type: GFButtonType.transparent,
-          ),
-        ],
-      ),
-      body: GFCarousel(
-        items: imageList.map(
-          (url) {
-            return Container(
-              margin: EdgeInsets.all(8.0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                child: Image.network(url, fit: BoxFit.cover, width: 1000.0),
-              ),
-            );
-          },
-        ).toList(),
-        onPageChanged: (index) {
-          setState(() {
-            index;
-          });
-        },
-      ),
-
-      // Card
-
-      // Bottom Navigation Tab
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Home'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            title: Text('Profile'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.feedback),
-            title: Text('Feed'),
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.blue[800],
-        onTap: _onItemTapped,
-      ),
+      home: FirstScreen(),
     );
   }
 }
